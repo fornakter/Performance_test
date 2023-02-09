6f24056de4596f231c8d2124016385218f1c89fd
AdamF <fornakter@gmail.com>
2023-02-09 17:11:47 +0100

    New version

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ import datetime
 result_table = []
 
 
-for a in range(5):
+for a in range(50):
     start = datetime.datetime.now()
-    for i in range(100000000):
+    for i in range(10000000):
         i += 1
     end = datetime.datetime.now()
     result = (end - start)
     print(f'Test nr {a}: {result}')
     result_table.append(result)
 
-for o in range(1, 5):
+for o in range(1, len(result_table)):
     result_table[0] += result_table[o]
 print(result_table[0])
 
